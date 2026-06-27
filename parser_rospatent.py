0bac3e4313059092cbee4e1facd5de1c11eeaec6
zhabaa <vova.dovnar2006@gmail.com>
2026-06-27 16:14:30 +0300

    add: base parser

--- parser_rospatent.py
+++ parser_rospatent.py
@@ -1,1 +1,18 @@
 # Парсер патентов с сайта роспатента для составления глоссария.
+# patents_parser_v1.py
+# Базовая структура парсера
+# TODO: Реализовать функциональность
+
+import time
+
+class PatentParser:
+    def __init__(self):
+        print("Инициализация парсера")
+    
+    def run(self):
+        print("Запуск парсера")
+        # TODO: Добавить логику парсинга
+
+if __name__ == "__main__":
+    parser = PatentParser()
+    parser.run()
