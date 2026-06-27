1e4f54bf4c04990689cedc151c0b8095149e49d3
zhabaa <vova.dovnar2006@gmail.com>
2026-06-27 16:15:48 +0300

    add: add selenium

--- parser_rospatent.py
+++ parser_rospatent.py
@@ -1,17 +1,34 @@
-# Парсер патентов с сайта роспатента для составления глоссария.
-# patents_parser_v1.py
-# Базовая структура парсера
-# TODO: Реализовать функциональность
+# patents_parser_v2.py
+# Добавлена инициализация Selenium WebDriver
 
+# TODO: Настроить пути к браузеру
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 
 class PatentParser:
     def __init__(self):
         print("Инициализация парсера")
+        self.driver = None
+    
+    def setup_driver(self):
+        """Инициализация Chrome драйвера"""
+
+        options = Options()
+        options.add_argument('--no-sandbox')
+        self.driver = webdriver.Chrome(options=options)
+
+        print("Драйвер запущен")
     
     def run(self):
+        self.setup_driver()
         print("Запуск парсера")
-        # TODO: Добавить логику парсинга
+
+        # TODO: Реализовать парсинг
+
+        time.sleep(2)
+        self.driver.quit()
 
 if __name__ == "__main__":
     parser = PatentParser()
